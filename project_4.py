8d284631f936284df51df8bcc34f2e1024211df7
Daniel-Lane-018 <127458641+Daniel-Lane-018@users.noreply.github.com>
2024-12-11 13:51:59 -0500

    Made the main execution code

--- project_4.py
+++ project_4.py
@@ -17,11 +17,11 @@ class Product:
     def final_price(self):
         return self._price * 0.10
     
-    class Electronics():
+    class Electronics(Product):
         def calculate_discount(self):
             return self._price * 0.10
         
-    class Clothing():
+    class Clothing(Product):
         def calculate_discount(self):
             return self._price * 0.20
 
@@ -52,3 +52,50 @@ class Order:
             print(f"-{product['name']}: ${product['final_price']:.2f}")
         print(f"Total: ${self._total:.2f}")
 
+# EXECUTION CODE
+class main():
+    products = [
+        Electronics("Smartphone", 699.99),
+        Clothing("Jeans", 49.99),
+        Electronics("Laptop", 1199.99),
+        Clothing("T-Shirt", 19.99),
+        Electronics("Headphones", 199.99),
+        Clothing("Jacket", 89.99)
+    ]
+
+    cart = ShoppingCart()
+
+    while True:
+        print("\nChoose an option:")
+        print("1. Add product to cart")
+        print("2. View cart")
+        print("3. Checkout")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            product_choice = int(input("Enter the product number to add to cart"))
+            if 1 <= product_choice <= len(products):
+                cart.add_product(products[product_choice - 1])
+                print(f"{products[product_choice - 1].name} added to cart.")
+            else:
+                print("invalid product number")
+        elif choice == "2":
+            print("\nCart Contents:")
+            for product in cart.list_contents():
+                print(f"- {product}")
+            print(f"Total: ${cart.calculate_total():.2f}")
+        elif choice == "3":
+            print("\nChecking out...")
+            order = Order(cart)
+            order.display_order_summary()
+            break
+        elif choice == "4":
+            print("Exiting Program. Have a nice day.")
+            break
+        else:
+            print("Invalid Choice")
+
+if __name__ == "__main__":
+    main()
